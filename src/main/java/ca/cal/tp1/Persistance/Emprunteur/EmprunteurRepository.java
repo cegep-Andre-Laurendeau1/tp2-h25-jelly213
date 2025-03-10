2f75e65cfbf3ebcee9dd387c26cb66bc193a40a8
Abdessalam Benmachiche <1952175@claurendeau.qc.ca>
2025-03-10 09:35:17 -0400

    ----------------------------------------- TP recommencé en Urgence donc ignorer les commit avant celui-ci.

--- src/main/java/ca/cal/tp1/Persistance/Emprunteur/EmprunteurRepository.java
+++
@@ -1,7 +0,0 @@
-package ca.cal.tp1.Persistance.Emprunteur;
-
-import ca.cal.tp1.Models.Emprunteur;
-
-public interface EmprunteurRepository {
-    void save(Emprunteur emprunteur);
-}
---
+++ src/main/java/ca/cal/tp2/Utils/TCPserver.java
@@ -0,0 +1,4 @@
+package ca.cal.tp2.Utils;
+
+public class TCPserver {
+}
